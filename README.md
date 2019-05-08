432ddd208325b17b7a3b8e6089c3cdbf2c7f3e13
Alex Eagleson <alexeagleson@gmail.com>
2019-05-08 12:55:02 -0400

    added dashboard example, removed dependency on reactstrap

--- README.md
+++ README.md
@@ -12,4 +12,22 @@ https://material-ui.com/
 
 ## remove
 
-final-form react-final-form reactstrap @types/reactstrap
+final-form react-final-form
+
+## need to add
+
+SCSS/SASS
+tests
+
+## hooks
+
+react hooks used for functional components
+
+react final form
+
+react fragments
+
+# dashboard dependencies
+recharts @types/classnames as an example of charting library - chart.js and victory also come highly recommended
+
+https://github.com/mui-org/material-ui/tree/master/docs/src/pages/getting-started/page-layout-examples/dashboard
--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,75 @@
 import React, { Component } from 'react';
+import Dashboard from './views/Dashboard';
+import PostViewer from './components/PostViewer';
+import PostEditor from './components/PostEditor';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
-import { Button, Container } from 'reactstrap';
-import PostViewer from './PostViewer';
-import PostEditor from './PostEditor';
-
-// const logo = require('./logo.svg');
-
-// const App: React.FC = () => {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// };
+const logo = require('./assets/logo.svg');
 
 class App extends Component {
   public state = {
+    viewMode: 1,
     editing: null,
   };
 
   public render() {
-    const { editing } = this.state;
+    const { viewMode, editing } = this.state;
 
-    return (
-      <Container fluid>
-        <Button
-          className="my-2"
-          color="primary"
-          onClick={() => this.setState({ editing: {} })}
-        >
-          New Post
-        </Button>
-        <PostViewer
-          canEdit={() => true}
-          onEdit={(post: any) => this.setState({ editing: post })}
-        />
-        {editing && (
-          <PostEditor
-            post={editing}
-            onClose={() => this.setState({ editing: null })}
-          />
-        )}
-      </Container>
-    );
+    if (viewMode === 1) {
+      return (
+        <React.Fragment>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => this.setState({ viewMode: 2 })}
+            >
+              Check out GraphQL!
+            </Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Dashboard />
+          </Grid>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <Grid item xs={12}>
+            <img src={logo} className="App-logo" alt="logo" />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => this.setState({ viewMode: 1 })}
+            >
+              Back to the Dashboard!
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.setState({ editing: {} })}
+            >
+              New Post
+            </Button>
+            <PostViewer
+              canEdit={() => true}
+              onEdit={(post: any) => this.setState({ editing: post })}
+            />
+            {editing && (
+              <PostEditor
+                post={editing}
+                onClose={() => this.setState({ editing: null })}
+              />
+            )}
+          </Grid>
+        </React.Fragment>
+      );
+    }
   }
 }
 
