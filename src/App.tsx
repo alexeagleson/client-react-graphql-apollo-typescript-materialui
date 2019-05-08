a27e5a27e32aa9a120570a19a0bada2973c4d29d
Alex Eagleson <alexeagleson@gmail.com>
2019-05-07 22:28:16 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button, Container } from 'reactstrap';
+import PostViewer from './PostViewer';
+import PostEditor from './PostEditor';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// const logo = require('./logo.svg');
+
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// };
+
+class App extends Component {
+  public state = {
+    editing: null,
+  };
+
+  public render() {
+    const { editing } = this.state;
+
+    return (
+      <Container fluid>
+        <Button
+          className="my-2"
+          color="primary"
+          onClick={() => this.setState({ editing: {} })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          New Post
+        </Button>
+        <PostViewer
+          canEdit={() => true}
+          onEdit={(post: any) => this.setState({ editing: post })}
+        />
+        {editing && (
+          <PostEditor
+            post={editing}
+            onClose={() => this.setState({ editing: null })}
+          />
+        )}
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+
+import client from './apollo';
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if ((module as any).hot) { (module as any).hot.accept(); }
